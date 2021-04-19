9aa21933f8ba42a444e79d7972b2484406cb3668
Mengyi Qian <47120003+mengyi-qian@users.noreply.github.com>
2021-04-19 00:17:38 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,20 +8,23 @@ function show(element) {
  
 let aboutButton = document.querySelector('#about')
 let aboutContainer = document.querySelector('#about-container')
-hide(aboutContainer)
+let navMessages = document.querySelector('nav.messages')
 aboutButton.addEventListener('click', () => {
   show(aboutContainer)
   hide(aboutButton)
+  hide(navMessages)
 })
 aboutContainer.addEventListener('click', () => {
   hide(aboutContainer)
   show(aboutButton)
+  show(navMessages)
 })
+
 
 // for mobile:
 function windowResize() {
   // get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
+  let vh = window.innerHeight * 0.01
   // set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty('--vh', `${vh}px`)
 }
